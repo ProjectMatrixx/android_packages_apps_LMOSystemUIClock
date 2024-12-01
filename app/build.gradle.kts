7be7d98f9ee4897f931a0bccaa3c140c0ebe26bb
Dhina17 <dhinalogu@gmail.com>
2024-12-01 11:43:18 +0530

    gradle: Update AGP and migrate to K2
    Change-Id: Ic6c1e109f31cbaf4ea6480dfc4a1360f7384d001

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
     defaultConfig {
         applicationId = "com.libremobileos.clock"
         minSdk = 34
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -31,11 +31,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
     signingConfigs {
         getByName("debug") {
